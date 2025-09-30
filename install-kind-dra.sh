c2110776482c12a90172836313612239c6dd0cf4
Rey Lejano <rlejano@gmail.com>
2025-09-30 13:17:38 -0700

    Update install-kind-dra.sh

--- install-kind-dra.sh
+++ install-kind-dra.sh
@@ -31,7 +31,7 @@ sudo yum install -y jq
 # install kind for AMD64/x86_64
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
 chmod +x ./kind
-mv ./kind /usr/local/bin/kind
+sudo mv ./kind /usr/local/bin/kind
 
 # download kubectl binary
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
