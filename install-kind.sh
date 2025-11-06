52db56b1d223392da611408559b73688d5d5bdf2
Rey Lejano <rlejano@gmail.com>
2025-11-06 11:09:05 -0800

    manifest updates

--- install-kind.sh
+++ install-kind.sh
@@ -46,9 +46,6 @@ kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-  extraPortMappings:
-  - containerPort: 11434
-    hostPort: 11434
 - role: worker
   extraPortMappings:
   - containerPort: 11434
